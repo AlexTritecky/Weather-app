fb58e1b9b18007ea03dc831617df94767c9c0b28
AlexTritecky <alextritecky@gmail.com>
2022-04-12 13:11:03 +0300

    deploy try 2

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
-const app = express();
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
 app.use(express.static(__dirname + "/dist/frontend"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/frontend/index.html"));
